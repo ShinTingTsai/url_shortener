765401ea601bf7bd2a6dcaa7469283704c11199a
STTSAI <shinting.tsai@gmail.com>
2020-08-08 02:14:22 +0800

    refactor: add routers folder

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 // use express
 const express = require("express");
 const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser");
-
+const bodyParser = require("body-parser")
+const routes = require("./routes");
+const router = require("./routes");
+require("./config/mongoose");
 const app = express();
-const port = 3000;
-require('./config/mongoose')
+const PORT = process.env.PORT || 3000;
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router)
 
 //setup router
-app.get("/", (req, res) => {
-  // res.send("hello world");
-  res.render("index");
-});
-// app.listen
-app.listen(port, () => {
-  console.log(`Express is running on http://localhost:${port}`);
+// app.get("/", (req, res) => {
+//   res.render("index");
+// })
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Express is running on http://localhost:${PORT}`);
 });
