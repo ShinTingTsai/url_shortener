ad822c55828af7e31864a538fb3cfd0d0cb8ac45
STTSAI <shinting.tsai@gmail.com>
2020-08-08 16:32:57 +0800

    feat: add init a url data and show/hind shortURL UI

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,33 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const URL = require('../models/url')
 
 router.get('/', (req, res) => {
   res.render('index')
 })
 
 
-router.get("/shorten", (req, res) => {
-  // res.render("index", { message: });
-  console.log('req.query', req.query)
-  const longURL = req.query.longURL
-  res.render("index", { msg: longURL});
-});
+router.post('/create', (req, res) => {
+  // console.log('req.query', req.query);
+  //check input date: repeat, null, format
+  //initial a url data
+  const url = req.body;
+  url.shortURL = 'testShortURL'
+  url.createdTime = new Date()
+  url.clicks = 0
+  console.log("url", url);
 
+  //cteate shorturl
+  return URL.create(url)
+    .then(() => res.render('index', { longURL: url.longURL, shortURL: url.shortURL}))
+    .catch((error) => console.error(error));
+  //check shorturl repeat
+  // write into DB
+
+  // show on browser 
+  // return URL.create(longURL)
+  // res.render("index", { msg: url });
+});
 
 
 module.exports = router;
